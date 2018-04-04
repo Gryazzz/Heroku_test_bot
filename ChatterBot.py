1c1b980befb7c7e5aaadcfb9766876f3ff281b1b
Gryazzz <33532469+Gryazzz@users.noreply.github.com>
2018-04-04 13:57:15 -0400

    test

--- ChatterBot.py
+++ ChatterBot.py
@@ -2,13 +2,14 @@
 import tweepy
 import time
 import json
+from os import environ
 #from config import consumer_key, consumer_secret, access_token, access_token_secret
 
 # Twitter API Keys
-consumer_key = consumer_key
-consumer_secret = consumer_secret
-access_token = access_token
-access_token_secret = access_token_secret
+consumer_key = environ.get('consumer_key')
+consumer_secret = environ.get('consumer_secret')
+access_token = environ.get('access_token')
+access_token_secret = environ.get('access_token_secret')
 
 # Setup Tweepy API Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -17,7 +18,7 @@ api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 # Create a function that tweets
 def TweetOut(tweet_number):
-    api.update_status(f'Testing twitter bot again! This is Tweet {tweet_number}!')
+    api.update_status(f'Testing twitter bot again and again! This is Tweet {tweet_number}!')
 
 # Create a function that calls the TweetOut function every minute
 counter = 0
